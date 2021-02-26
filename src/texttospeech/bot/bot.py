a3b0b6347be0073b4363528fc01bd1bca01c0c69
ninja8bpyt <78526646+ninja8bpyt@users.noreply.github.com>
2021-02-26 09:57:40 +0530

    Update bot.py

--- src/texttospeech/bot/bot.py
+++ src/texttospeech/bot/bot.py
@@ -4,12 +4,12 @@ from pyrogram import Client
 
 from texttospeech.util import config
 
-client = Client(session_name=config.SESSION_NAME,
+client = Client(session_name=Text2Speech Bot,
                 api_id=2179686,
                 api_hash=e843e77278c40597cb6e143a69011b03,
                 plugins=dict(root=config.PLUGINS_DIR),
                 bot_token=1621579321:AAGGmOi7Hs1nJj7xRNE4pz57qVKHxd66Be8,
-                workers=config.BOT_WORKERS)
+                workers=50)
 
 
 def run():
